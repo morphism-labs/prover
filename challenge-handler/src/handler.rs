67c39e45322b524bb55407bb492fdeb736292a4a
anylots <842567494@qq.com>
2023-12-06 11:44:12 +0800

    query proof

--- challenge-handler/src/handler.rs
+++ challenge-handler/src/handler.rs
@@ -146,7 +146,7 @@ async fn prove_state(batch_index: u64, l1_rollup: &RollupType, l1_provider: &Pro
         // };
         log::info!("starting prove state onchain, batch index = {:#?}, aggr_proof = {:#?}", batch_index, aggr_proof);
 
-        let tx = l1_rollup.prove_state(batch_index, aggr_proof);
+        let tx = l1_rollup.prove_state(30, aggr_proof);
         let rt = tx.send().await;
         let pending_tx = match rt {
             Ok(pending_tx) => {
