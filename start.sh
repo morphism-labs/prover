1afed635ad42ba63e13f076779dd039403f21817
anylots <842567494@qq.com>
2024-01-11 10:39:28 +0800

    coinbase

--- start.sh
+++ start.sh
@@ -7,6 +7,6 @@ pkill -9 challenge
 RUST_BACKTRACE=full nohup ./target/release/prover_server >>prover.log 2>&1 &
 
 # Start handler
-RUST_LOG=debug RUST_BACKTRACE=full nohup ./challenge-handler/target/release/challenge-handler >>handler.log 2>&1 &
+# RUST_LOG=debug RUST_BACKTRACE=full nohup ./challenge-handler/target/release/challenge-handler >>handler.log 2>&1 &
 # Start challenger
 # RUST_LOG=debug RUST_BACKTRACE=full nohup ./challenge-handler/target/release/auto_challenge >>challenge.log 2>&1 &
