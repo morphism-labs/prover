027697eaf109554031ad079e2ad56f00c8667142
anylots <842567494@qq.com>
2024-02-07 17:47:19 +0800

    4844 zkp

--- src/prover.rs
+++ src/prover.rs
@@ -114,7 +114,7 @@ async fn generate_proof(batch_index: u64, chunk_traces: Vec<Vec<BlockTrace>>, ch
     let mut pre: Vec<u8> = vec![];
     pre.extend(batch_commit.to_le_bytes().to_vec());
     pre.extend(batch_blob);
-    let challenge_point = U256::from_little_endian(keccak256(pre.as_slice()).as_ref());
+    let challenge_point = U256::from(128);
 
     let mut index = 0;
     for chunk_trace in chunk_traces.iter(){
