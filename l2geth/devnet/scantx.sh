57cbc600e0ac1b945236e383cc8856ef0b6cd53b
anylots <842567494@qq.com>
2024-02-27 23:17:45 +0800

    scantx

---
+++ l2geth/devnet/scantx.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+block_number=0x2
+
+while true; do
+    data='{"jsonrpc": "2.0", "method": "eth_getBlockTransactionCountByNumber", "params": ["'$block_number'"], "id": 1}'
+    
+    response=$(curl -s -X POST -H "Content-Type: application/json" -d "$data" http://localhost:6688/)
+    
+    tx_count=$(echo $response | jq -r '.result')
+    
+    if [ "$tx_count" != "0x0" ]; then
+        echo "Block $block_number has $tx_count transactions."
+        break
+    else
+        echo "Block $block_number has no transactions."
+    fi
+    
+    block_number=$(printf "0x%x" $(($block_number + 1)))
+done
