d567ea9819d29609c28675962d7b0517e2db6500
anylots <842567494@qq.com>
2024-03-08 15:00:04 +0800

    and kzgComment

--- src/prover.rs
+++ src/prover.rs
@@ -130,9 +130,9 @@ async fn generate_proof(batch_index: u64, chunk_traces: Vec<Vec<BlockTrace>>, ch
     pre.extend(commitment.to_bytes().to_vec());
     pre.extend(batch_data_hash);
     let challenge_point: U256 = U256::from_little_endian(keccak256(pre.as_slice()).as_ref());
-    // let challenge_point = U256::from(128);
     let mut challenge_point_bytes = challenge_point.to_le_bytes();
     challenge_point_bytes[0] = 0;
+    // let challenge_point = U256::from(128);
 
     let (proof, y) = match KzgProof::compute_kzg_proof(
         &Blob::from_bytes(&batch_blob).unwrap(),
